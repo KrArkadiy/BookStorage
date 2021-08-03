bcd9ed81b5f5ebe321ceb208c35c3ecbc39bbb72
arkadiy krylosov <arkady.krylosov@yandex.ru>
2021-08-03 04:57:42 +0300

    LabelViewTest realized

--- src/test/java/view/LabelViewTest.java
+++ src/test/java/view/LabelViewTest.java
@@ -1,31 +1,90 @@
 package view;
 
 import controller.LabelController;
+import model.Label;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import utility.JdbcTemplate;
 
-class LabelViewTest extends UnitBase{
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class LabelViewTest {
 
     private final LabelController LABEL_CONTROLLER = new LabelController();
 
     @Test
-    void getById() {
+    void givenId_whenRun_thenReturnLabelName() {
+        try{
+            Class.forName(JdbcTemplate.getJdbcDriver());
+            Connection connection = DriverManager.getConnection(JdbcTemplate.getDatabaseUrl(), JdbcTemplate.getUSER(), JdbcTemplate.getPASSWORD());
 
-
+            Assertions.assertEquals("FirstLabel", LABEL_CONTROLLER.getById(1L).getName());
+            Assertions.assertNotEquals("ThirdLabel", LABEL_CONTROLLER.getById(1L).getName());
+        } catch (ClassNotFoundException | SQLException exception){
+            exception.getStackTrace();
+        }
     }
 
     @Test
     void getAll() {
+        try{
+            Class.forName(JdbcTemplate.getJdbcDriver());
+            Connection connection = DriverManager.getConnection(JdbcTemplate.getDatabaseUrl(), JdbcTemplate.getUSER(), JdbcTemplate.getPASSWORD());
+            List<Label> labels = LABEL_CONTROLLER.getAll();
+            Assertions.assertEquals("SecondLabel", labels.get(0).getName());
+            Assertions.assertEquals("FirstLabel", labels.get(1).getName());
+            Assertions.assertEquals("ThirdLabel", labels.get(2).getName());
+        } catch (ClassNotFoundException | SQLException exception){
+            exception.getStackTrace();
+        }
     }
 
     @Test
     void deleteById() {
+        try{
+            Class.forName(JdbcTemplate.getJdbcDriver());
+            Connection connection = DriverManager.getConnection(JdbcTemplate.getDatabaseUrl(), JdbcTemplate.getUSER(), JdbcTemplate.getPASSWORD());
+            LABEL_CONTROLLER.deleteById(1L);
+            List<Label> labels = LABEL_CONTROLLER.getAll();
+            Assertions.assertEquals("SecondLabel", labels.get(0).getName());
+            Assertions.assertEquals("ThirdLabel", labels.get(1).getName());
+        } catch (ClassNotFoundException | SQLException exception){
+            exception.getStackTrace();
+        }
     }
 
     @Test
     void save() {
+        try{
+            Class.forName(JdbcTemplate.getJdbcDriver());
+            Connection connection = DriverManager.getConnection(JdbcTemplate.getDatabaseUrl(), JdbcTemplate.getUSER(), JdbcTemplate.getPASSWORD());
+            List<Label> labels = LABEL_CONTROLLER.getAll();
+            LABEL_CONTROLLER.save(new Label(1, "ForthLabel"));
+            Assertions.assertEquals("SecondLabel", labels.get(0).getName());
+            Assertions.assertEquals("FirstLabel", labels.get(1).getName());
+            Assertions.assertEquals("ThirdLabel", labels.get(2).getName());
+            Assertions.assertEquals("ForthLabel", labels.get(3).getName());
+        } catch (ClassNotFoundException | SQLException exception){
+            exception.getStackTrace();
+        }
     }
 
-    @Test
+/*    @Test
     void update() {
-    }
+        try{
+            Class.forName(JdbcTemplate.getJdbcDriver());
+            Connection connection = DriverManager.getConnection(JdbcTemplate.getDatabaseUrl(), JdbcTemplate.getUSER(), JdbcTemplate.getPASSWORD());
+            Label label = new Label(1, "ForthLabel");
+            LABEL_CONTROLLER.update(label);
+            List<Label> labels = LABEL_CONTROLLER.getAll();
+            Assertions.assertEquals("SecondLabel", labels.get(0).getName());
+            Assertions.assertEquals("ThirdLabel", labels.get(1).getName());
+            Assertions.assertEquals("FirstLabel", labels.get(2).getName());
+        } catch (ClassNotFoundException | SQLException exception){
+            exception.getStackTrace();
+        }
+    }*/
 }
