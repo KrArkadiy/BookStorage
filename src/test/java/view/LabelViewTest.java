c4a13ab4b628a772fff904c6cd0f05898e15566c
arkadiy krylosov <arkady.krylosov@yandex.ru>
2021-08-03 03:48:21 +0300

    Style corrected

---
+++ src/test/java/view/LabelViewTest.java
@@ -0,0 +1,31 @@
+package view;
+
+import controller.LabelController;
+import org.junit.jupiter.api.Test;
+
+class LabelViewTest extends UnitBase{
+
+    private final LabelController LABEL_CONTROLLER = new LabelController();
+
+    @Test
+    void getById() {
+
+
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+}
---
+++ src/test/java/view/PostViewTest.java
@@ -0,0 +1,28 @@
+package view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostViewTest {
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+}
