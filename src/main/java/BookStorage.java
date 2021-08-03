11c4b69c3b0990f1f4b25c56f7dc675ada988b34
arkadiy krylosov <arkady.krylosov@yandex.ru>
2021-08-03 03:16:01 +0300

    Entry point added

---
+++ src/main/java/BookStorage.java
@@ -0,0 +1,11 @@
+
+import view.ConsoleFunctionality;
+
+import java.sql.SQLException;
+
+public class BookStorage {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ConsoleFunctionality runner = new ConsoleFunctionality();
+        runner.run();
+    }
+}
